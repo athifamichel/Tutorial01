6af454d9ada795a71515e42940520d13d7580d74
kelompok 10 TK <athifa.am@gmail.com>
2018-09-07 18:50:11 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,24 @@
+
+public class Mahasiswa {
+	String nama;
+	int npm;
+	
+	public Mahasiswa(String nama, int npm) {
+		super();
+		this.nama = nama;
+		this.npm = npm;
+	}
+	
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public int getNpm() {
+		return npm;
+	}
+	public void setNpm(int npm) {
+		this.npm = npm;
+	}
+}
