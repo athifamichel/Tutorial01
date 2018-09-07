ca4e9a0352b2e5c34a692b22b2731e715204f93f
kelompok 10 TK <athifa.am@gmail.com>
2018-09-07 18:58:50 +0700

    Initial Commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,28 @@
+
+public class Dosen {
+	String nama;
+	int nip;
+	
+	public Dosen(String nama, int nip) {
+		super();
+		this.nama = nama;
+		this.nip = nip;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public int getNip() {
+		return nip;
+	}
+
+	public void setNip(int nip) {
+		this.nip = nip;
+	}
+	
+}
